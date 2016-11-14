e6fa7d3e96fce6ce53c6066a56aac3813befa86a
edgarkhanzadyan <edgar.khanzadyan@yandex.ru>
2016-11-14 23:03:08 +0300

    mobUpd

---
+++ footNavMobile.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  let open = false;
+	$('.item_nav_foot_mob1').click(function(){
+    $('.item_nav_foot_unvis1').toggleClass('item_nav_foot_vis1');
+  });
+  $('.item_nav_foot_mob2').click(function(){
+    $('.item_nav_foot_unvis2').toggleClass('item_nav_foot_vis2');
+  });
+  $('.item_nav_foot_mob3').click(function(){
+    $('.item_nav_foot_unvis3').toggleClass('item_nav_foot_vis3');
+  });
+  $('.item_nav_foot_mob4').click(function(){
+    $('.item_nav_foot_unvis4').toggleClass('item_nav_foot_vis4');
+  });
+  $('.item_nav_foot_mob5').click(function(){
+    $('.item_nav_foot_unvis5').toggleClass('item_nav_foot_vis5');
+  });
+  $('.item_nav_foot_mob6').click(function(){
+    $('.item_nav_foot_unvis6').toggleClass('item_nav_foot_vis6');
+  });
+  $('.item_nav_foot_mob7').click(function(){
+    $('.item_nav_foot_unvis7').toggleClass('item_nav_foot_vis7');
+  });
+});
